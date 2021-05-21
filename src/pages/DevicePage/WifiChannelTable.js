f3dd85c4c772f8bdbfddcd1bb513a4ef8318e005
bourquecharles <charles.bourque96@gmail.com>
2021-05-21 17:41:48 -0400

    Wifi channel table now sorts by channel

--- src/pages/DevicePage/WifiChannelTable.js
+++ src/pages/DevicePage/WifiChannelTable.js
@@ -2,30 +2,40 @@ import {
     CCol,
     CRow
   } from '@coreui/react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { v4 as createUuid } from 'uuid';
 import WifiChannelCard from './WifiChannelCard';
 
-const WifiChannelTable = ({channels}) => (
-    <CRow>
-        <CCol>
-            {
-                channels.map((channel, index) => {
-                    if(index % 2 === 0) return <WifiChannelCard key={createUuid()} channel={channel}/>;
-                    return <></>
-                })
-            }
-        </CCol>
-        <CCol>
-            {
-                channels.map((channel, index) => {
-                    if(index % 2 === 1) return <WifiChannelCard key={createUuid()} channel={channel}/>;
-                    return <></>
-                })
-            }
-        </CCol>
-    </CRow>
-)
+const WifiChannelTable = ({channels}) => {
+    const sortChannels = () => {
+        channels.sort((a, b) => (a.channel > b.channel) ? 1 : -1);
+    };
 
+    useEffect(() => {
+        if (channels)
+            sortChannels();
+    }, [channels]);
+
+    return (
+        <CRow>
+            <CCol>
+                {
+                    channels.map((channel, index) => {
+                        if(index % 2 === 0) return <WifiChannelCard key={createUuid()} channel={channel}/>;
+                        return <></>
+                    })
+                }
+            </CCol>
+            <CCol>
+                {
+                    channels.map((channel, index) => {
+                        if(index % 2 === 1) return <WifiChannelCard key={createUuid()} channel={channel}/>;
+                        return <></>
+                    })
+                }
+            </CCol>
+        </CRow>
+    );
+}
 
 export default WifiChannelTable;
