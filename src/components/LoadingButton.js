d860afc4816d6fc342f2c3309b7ec826dbed0ad8
bourquecharles <charles.bourque96@gmail.com>
2021-05-30 10:36:16 -0400

    Added View More button

--- src/components/LoadingButton.js
+++ src/components/LoadingButton.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CButton, CSpinner } from '@coreui/react';
 
-const LoadingButton = ({ isLoading, label, isLoadingLabel, action }) => (
-  <CButton disabled={isLoading} color="primary" onClick={() => action()} block>
+const LoadingButton = ({ isLoading, label, isLoadingLabel, action, color, variant}) => (
+  <CButton variant={variant} disabled={isLoading} color={color} onClick={() => action()} block>
     {isLoading ? isLoadingLabel : label}
     <CSpinner hidden={!isLoading} component="span" size="sm" />
   </CButton>
@@ -14,6 +14,13 @@ LoadingButton.propTypes = {
   label: PropTypes.string.isRequired,
   isLoadingLabel: PropTypes.string.isRequired,
   action: PropTypes.func.isRequired,
+  color: PropTypes.string,
+  variant: PropTypes.string
 };
+
+LoadingButton.defaultProps = {
+  color: "primary",
+  variant: ""
+}
 
 export default LoadingButton;
