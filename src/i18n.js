46f3bfc7a4cc0442f86cb963fc63436f5d539a33
bourquecharles <charles.bourque96@gmail.com>
2021-06-15 20:55:31 -0400

    Added cookie detector for i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,15 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18next
   .use(initReactI18next)
   .use(HttpApi)
+  .use(LanguageDetector)
   .init({
     supportedLngs: ['en', 'fr', 'es', 'de', 'pt'],
     fallbackLng: 'en',
-    lng: 'en',
     interpolation: {
       escapeValue: false,
     },
