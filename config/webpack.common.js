a422d5d4469364cb202e4ec61b5964092c3a08f7
bourquecharles <charles.bourque96@gmail.com>
2021-07-12 15:20:55 -0400

    Webpack and dependency cleanup

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -8,10 +8,7 @@ const path = require('path');
 const paths = require('./paths');
 
 module.exports = {
-  // Where webpack looks to start building the bundle
   entry: [paths.src + '/index.js'],
-
-  // Where webpack outputs the assets and bundles
   output: {
     path: paths.build,
     filename: '[name].bundle.js',
@@ -21,16 +18,13 @@ module.exports = {
     modules: [path.resolve('./node_modules'), path.resolve('./src')],
     preferRelative: true,
   },
-  // Customize the webpack build process
   plugins: [
-    // Removes/cleans build folders and unused assets when rebuilding
     new CleanWebpackPlugin(),
 
     new MiniCssExtractPlugin({
       filename: 'styles/[name].[contenthash].css',
       chunkFilename: '[id].[contenthash].css',
     }),
-    // Copies files from target to destination folder
     new CopyWebpackPlugin({
       patterns: [
         {
@@ -53,14 +47,11 @@ module.exports = {
         },
       ],
     }),
-
-    // Generates an HTML file from a template
-    // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
       title: 'uCentralGW',
       favicon: paths.public + '/favicon.ico',
-      template: paths.public + '/index.html', // template file
-      filename: 'index.html', // output file
+      template: paths.public + '/index.html',
+      filename: 'index.html',
     }),
   ],
 
@@ -71,18 +62,14 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
-
       {
         test: /\.(css|scss)$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
-
       {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
       },
-
-      // Images: Copy image files to build folder
       { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
     ],
   },
