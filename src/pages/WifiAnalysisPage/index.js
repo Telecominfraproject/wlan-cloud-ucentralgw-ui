e23b77c400728413628de6550f11f7607df1d743
bourquecharles <charles.bourque96@gmail.com>
2021-07-22 16:03:01 -0400

    Radio analysis table added

--- src/pages/WifiAnalysisPage/index.js
+++ src/pages/WifiAnalysisPage/index.js
@@ -1,32 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
-import { WifiAnalysisTable } from 'ucentral-libs';
+import { WifiAnalysisTable, RadioAnalysisTable } from 'ucentral-libs';
 import { useAuth } from 'contexts/AuthProvider';
 import axiosInstance from 'utils/axiosInstance';
-import { cleanBytesString, prettyDate } from 'utils/helper';
-import { CCard, CCardBody } from '@coreui/react';
+import { cleanBytesString, prettyDate, secondsToDetailed } from 'utils/helper';
+import { CCard, CCardBody, CCardHeader, CCol, CRow } from '@coreui/react';
 
 const WifiAnalysisPage = () => {
   const { t } = useTranslation();
   const { deviceId } = useParams();
   const { currentToken, endpoints } = useAuth();
   const [loading, setLoading] = useState(false);
-  const [parsedStats, setParsedStats] = useState([]);
-  const [selectedStats, setSelectedStats] = useState([]);
+  const [tableTime, setTableTime] = useState('');
+  const [parsedAssociationStats, setParsedAssociationStats] = useState([]);
+  const [selectedAssociationStats, setSelectedAssociationStats] = useState([]);
+  const [parsedRadioStats, setParsedRadioStats] = useState([]);
+  const [selectedRadioStats, setSelectedRadioStats] = useState([]);
   const [range, setRange] = useState(19);
 
   const extractIp = (json, bssid) => {
     const ips = {
-      ipV4: '',
-      ipV6: '',
+      ipV4: '-',
+      ipV6: '-',
     };
     for (const obj of json.interfaces) {
       if ('clients' in obj) {
         for (const client of obj.clients) {
           if (client.mac === bssid) {
-            ips.ipV4 = client.ipv4_addresses ?? '';
-            ips.ipV6 = client.ipv6_addresses ?? '';
+            ips.ipV4 = client.ipv4_addresses ?? '-';
+            ips.ipV6 = client.ipv6_addresses ?? '-';
           }
         }
       }
@@ -34,13 +37,61 @@ const WifiAnalysisPage = () => {
     return ips;
   };
 
-  const parseStats = (json) => {
+  const parseAssociationStats = (json) => {
     const dbmNumber = 4294967295;
-    const newParsedStats = [];
+    const newParsedAssociationStats = [];
+    const newParsedRadioStats = [];
 
     for (const stat of json.data) {
       const associations = [];
+      const radios = [];
       const timeStamp = prettyDate(stat.recorded);
+
+      for (let i = 0; i < stat.data.radios.length; i += 1) {
+        const radio = stat.data.radios[i];
+        radios.push({
+          timeStamp,
+          radio: i,
+          channel: radio.channel,
+          channelWidth: radio.channel_width,
+          noise: radio.noise ? (dbmNumber - radio.noise) * -1 : '-',
+          txPower: radio.tx_power,
+          activeMs: secondsToDetailed(
+            radio?.active_ms ? Math.floor(radio.active_ms / 1000) : 0,
+            t('common.day'),
+            t('common.days'),
+            t('common.hour'),
+            t('common.hours'),
+            t('common.minute'),
+            t('common.minutes'),
+            t('common.second'),
+            t('common.seconds'),
+          ),
+          busyMs: secondsToDetailed(
+            radio?.busy_ms ? Math.floor(radio.busy_ms / 1000) : 0,
+            t('common.day'),
+            t('common.days'),
+            t('common.hour'),
+            t('common.hours'),
+            t('common.minute'),
+            t('common.minutes'),
+            t('common.second'),
+            t('common.seconds'),
+          ),
+          receiveMs: secondsToDetailed(
+            radio?.receive_ms ? Math.floor(radio.receive_ms / 1000) : 0,
+            t('common.day'),
+            t('common.days'),
+            t('common.hour'),
+            t('common.hours'),
+            t('common.minute'),
+            t('common.minutes'),
+            t('common.second'),
+            t('common.seconds'),
+          ),
+        });
+      }
+      newParsedRadioStats.push(radios);
 
       // Looping through the interfaces
       for (const deviceInterface of stat.data.interfaces) {
@@ -61,13 +112,14 @@ const WifiAnalysisPage = () => {
                 const data = {
                   ...radioInfo,
                   ...extractIp(stat.data, association.bssid),
+                  bssid: association.bssid,
                   ssid: ssid.ssid,
                   rssi: (dbmNumber - association.rssi) * -1,
                   mode: ssid.mode,
                   rxBytes: cleanBytesString(association.rx_bytes, 0),
                   rxRate: association.rx_rate.bitrate,
-                  rxMcs: association.rx_rate.mcs ?? '',
-                  rxNss: association.rx_rate.nss ?? '',
+                  rxMcs: association.rx_rate.mcs ?? '-',
+                  rxNss: association.rx_rate.nss ?? '-',
                   txBytes: cleanBytesString(association.tx_bytes, 0),
                   txMcs: association.tx_rate.mcs,
                   txNss: association.tx_rate.nss,
@@ -80,16 +132,21 @@ const WifiAnalysisPage = () => {
           }
         }
       }
-      newParsedStats.push(associations);
+      newParsedAssociationStats.push(associations);
     }
-    const ascOrderedStats = newParsedStats.reverse();
-    setRange(ascOrderedStats.length - 1);
-    setParsedStats(ascOrderedStats);
-    setSelectedStats(ascOrderedStats[ascOrderedStats.length - 1]);
+
+    const ascOrderedRadioStats = newParsedRadioStats.reverse();
+    setParsedRadioStats(ascOrderedRadioStats);
+    setSelectedRadioStats(ascOrderedRadioStats[ascOrderedRadioStats.length - 1]);
+    setRange(ascOrderedRadioStats.length - 1);
+    setTableTime(ascOrderedRadioStats[ascOrderedRadioStats.length - 1][0].timeStamp);
+    const ascOrderedAssociationStats = newParsedAssociationStats.reverse();
+    setParsedAssociationStats(ascOrderedAssociationStats);
+    setSelectedAssociationStats(ascOrderedAssociationStats[ascOrderedAssociationStats.length - 1]);
     setLoading(false);
   };
 
-  const getLatestStats = () => {
+  const getLatestAssociationStats = () => {
     setLoading(true);
     setRange(19);
 
@@ -106,33 +163,57 @@ const WifiAnalysisPage = () => {
         options,
       )
       .then((response) => {
-        parseStats(response.data);
+        parseAssociationStats(response.data);
       })
-      .catch(() => {
+      .catch((e) => {
+        console.log(e);
         setLoading(false);
       });
   };
 
   const updateSelectedStats = (index) => {
-    setSelectedStats(parsedStats[index]);
+    setTableTime(parsedRadioStats[index][0].timeStamp);
+    setSelectedAssociationStats(parsedAssociationStats[index]);
+    setSelectedRadioStats(parsedRadioStats[index]);
   };
 
   useEffect(() => {
     if (deviceId && deviceId.length > 0) {
-      getLatestStats();
+      getLatestAssociationStats();
     }
   }, [deviceId]);
 
   return (
     <div>
       <CCard>
+        <CCardHeader>
+          <CRow>
+            <CCol className="text-center">
+              <input
+                type="range"
+                style={{ width: '80%' }}
+                className="form-range"
+                min="0"
+                max={range}
+                step="1"
+                onChange={(e) => updateSelectedStats(e.target.value)}
+                defaultValue={range}
+              />
+              <h5>
+                {t('common.timestamp')}: {tableTime}
+              </h5>
+            </CCol>
+          </CRow>
+        </CCardHeader>
         <CCardBody>
+          <h5 className="pb-3 text-center">{t('wifi_analysis.radios')}</h5>
+          <RadioAnalysisTable data={selectedRadioStats} loading={loading} range={range} />
+          <h5 className="pt-5 pb-3 text-center">{t('wifi_analysis.associations')}</h5>
           <WifiAnalysisTable
             t={t}
-            data={selectedStats}
+            data={selectedAssociationStats}
             loading={loading}
             range={range}
-            updateSelectedStats={updateSelectedStats}
           />
         </CCardBody>
       </CCard>
