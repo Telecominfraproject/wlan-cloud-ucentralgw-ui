69e5c2ef48f18a9c8c07b31e77a310f38b9cb0fb
BourqueCharles <charles.bourque96@gmail.com>
2021-08-12 14:26:13 -0400

    Version 2.0.22

--- src/layout/index.js
+++ src/layout/index.js
@@ -89,7 +89,7 @@ const TheLayout = () => {
             <PageContainer t={t} routes={routes} redirectTo="/devices" />
           </ToastProvider>
         </div>
-        <Footer t={t} version="2.0.20" />
+        <Footer t={t} version="2.0.22" />
       </div>
     </div>
   );
