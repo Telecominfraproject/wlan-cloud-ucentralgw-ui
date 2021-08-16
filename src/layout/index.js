9fb2c92a15c89767baf3fb65b8f08e8194b48b9b
BourqueCharles <charles.bourque96@gmail.com>
2021-08-16 15:21:19 -0400

    Added LindsayBB logo

--- src/layout/index.js
+++ src/layout/index.js
@@ -66,7 +66,7 @@ const TheLayout = () => {
       <Sidebar
         showSidebar={showSidebar}
         setShowSidebar={setShowSidebar}
-        logo="assets/OpenWiFi_LogoLockup_WhiteColour.svg"
+        logo="assets/LindsayBB_Logo.png"
         options={navigation}
         redirectTo="/devices"
       />
@@ -78,7 +78,7 @@ const TheLayout = () => {
           t={t}
           i18n={i18n}
           logout={logout}
-          logo="assets/OpenWiFi_LogoLockup_DarkGreyColour.svg"
+          logo="assets/LindsayBB_Logo.png"
           authToken={currentToken}
           endpoints={endpoints}
           user={user}
--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -295,6 +295,7 @@ const Login = () => {
       updateField={updateFieldWithId}
       toggleForgotPassword={toggleForgotPassword}
       isLogin={isLogin}
+      logo="assets/LindsayBB_Logo.png"
       isPasswordChange={isPasswordChange}
       onKeyDown={onKeyDown}
       sendForgotPasswordEmail={sendForgotPasswordEmail}
