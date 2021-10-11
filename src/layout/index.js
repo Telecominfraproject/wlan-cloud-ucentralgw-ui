8b91d3c7e57841bdf4da4a0efec210c2604a752d
Charles <charles.bourque96@gmail.com>
2021-10-11 15:20:15 -0400

    JoinDigital branding

--- src/layout/index.js
+++ src/layout/index.js
@@ -66,7 +66,7 @@ const TheLayout = () => {
       <Sidebar
         showSidebar={showSidebar}
         setShowSidebar={setShowSidebar}
-        logo="assets/Viasat_logo_dark.png"
+        logo="assets/JoinDigital_Logo.png"
         options={navigation}
         redirectTo="/devices"
       />
@@ -78,7 +78,7 @@ const TheLayout = () => {
           t={t}
           i18n={i18n}
           logout={logout}
-          logo="assets/Viasat_logo_dark.png"
+          logo="assets/JoinDigital_Logo.png"
           authToken={currentToken}
           endpoints={endpoints}
           user={user}
