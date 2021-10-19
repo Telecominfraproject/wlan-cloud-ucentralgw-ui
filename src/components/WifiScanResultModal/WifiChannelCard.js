65ffc7a6568af0c6cd0bbbffc8d0fbdfff25c0ad
Charles <charles.bourque96@gmail.com>
2021-10-19 11:11:40 -0400

    WifiAnalysis fix

--- src/components/WifiScanResultModal/WifiChannelCard.js
+++ src/components/WifiScanResultModal/WifiChannelCard.js
@@ -4,6 +4,12 @@ import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import 'react-widgets/styles.css';
 
+const parseDbm = (value) => {
+  if (!value) return '-';
+  if (value > -150 && value < 100) return value;
+  return 4294967295 + value;
+};
+
 const WifiChannelCard = ({ channel }) => {
   const { t } = useTranslation();
   const columns = [{ key: 'SSID', _style: { width: '70%' } }, { key: 'Signal' }];
@@ -17,7 +23,14 @@ const WifiChannelCard = ({ channel }) => {
       </CCardHeader>
       <CCardBody>
         <div className="overflow-auto" style={{ height: '250px' }}>
-          <CDataTable items={channel.devices} fields={columns} className="text-white" />
+          <CDataTable
+            items={channel.devices}
+            fields={columns}
+            className="text-white"
+            scopedSlots={{
+              Signal: (item) => <td>{parseDbm(item.Signal)}</td>,
+            }}
+          />
         </div>
       </CCardBody>
     </CCard>
