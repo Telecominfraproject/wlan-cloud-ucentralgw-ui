e0ba4e4b20ad0ef8a6cc4d082082f08097ee5063
Charles <charles.bourque96@gmail.com>
2021-10-19 09:51:41 -0400

    Wifi Analysis fix for empty rssi/tx values

--- src/pages/WifiAnalysisPage/index.js
+++ src/pages/WifiAnalysisPage/index.js
@@ -22,6 +22,7 @@ import CIcon from '@coreui/icons-react';
 import { cilX } from '@coreui/icons';
 
 const parseDbm = (value) => {
+  if (!value) return '-';
   if (value > -150 && value < 100) return value;
   return (4294967295 - value) * -1;
 };
@@ -82,7 +83,7 @@ const WifiAnalysisPage = () => {
             channel: radio.channel,
             channelWidth: radio.channel_width,
             noise: radio.noise ? parseDbm(radio.noise) : '-',
-            txPower: radio.tx_power,
+            txPower: radio.tx_power ?? '-',
             activeMs: secondsToLabel(radio?.active_ms ? Math.floor(radio.active_ms / 1000) : 0),
             busyMs: secondsToLabel(radio?.busy_ms ? Math.floor(radio.busy_ms / 1000) : 0),
             receiveMs: secondsToLabel(radio?.receive_ms ? Math.floor(radio.receive_ms / 1000) : 0),
