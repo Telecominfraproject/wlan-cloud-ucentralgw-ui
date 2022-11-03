8c20d41d89e1062c73adda2abf59c4a35b04bcc4
Dmitry Dunaev <dmitry@opsfleet.com>
2022-11-03 10:33:52 +0300

    [WIFI-11464] Fix: entrypoint script
    Signed-off-by: Dmitry Dunaev <dmitry@opsfleet.com>

--- docker-entrypoint.d/40-generate-config.sh
+++ docker-entrypoint.d/40-generate-config.sh
@@ -1,6 +1,32 @@
 #!/bin/ash
-# Check if variables are set
-export DEFAULT_OWSEC_URL="${DEFAULT_OWSEC_URL:-https://ucentral.dpaas.arilia.com:16001}"
-export ALLOW_OWSEC_CHANGE="${ALLOW_OWSEC_CHANGE:-false}"
 
-echo '{"DEFAULT_UCENTRALSEC_URL": "'$DEFAULT_UCENTRALSEC_URL'","ALLOW_UCENTRALSEC_CHANGE": '$ALLOW_UCENTRALSEC_CHANGE'}' > /usr/share/nginx/html/config.json
+ENV_CONFIG_PATH=/usr/share/nginx/html/env-config.js
+
+# Recreate config file
+rm -rf $ENV_CONFIG_PATH
+touch $ENV_CONFIG_PATH
+
+# Add assignment
+echo "window._env_ = {" >> $ENV_CONFIG_PATH
+
+# Read each line in .env file
+# Each line represents key=value pairs
+env | grep REACT_ | while read -r line || [[ -n "$line" ]];
+do
+  echo $line
+  # Split env variables by character `=`
+  if printf '%s\n' "$line" | grep -q -e '='; then
+    varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
+    varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+  fi
+
+  # Read value of current variable if exists as Environment variable
+  value=$(printf '%s\n' "${!varname}")
+  # Otherwise use value from .env file
+  [[ -z $value ]] && value=${varvalue}
+
+  # Append configuration property to JS file
+  echo "  $varname: \"$value\"," >> $ENV_CONFIG_PATH
+done
+
+echo "}" >> $ENV_CONFIG_PATH
