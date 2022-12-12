48654c382d8689edac2333138ed6a5cd06ebe91c
Charles <charles.bourque96@gmail.com>
2022-12-12 16:37:06 -0500

    [WIFI-11958] Device restriction details display
    Signed-off-by: Charles <charles.bourque96@gmail.com>

---
+++ src/pages/Device/RestrictionsCard/index.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { Box, Flex, Heading, ListItem, Text, UnorderedList } from '@chakra-ui/react';
+import { LockSimple, LockSimpleOpen } from 'phosphor-react';
+import { useTranslation } from 'react-i18next';
+import { Card } from 'components/Containers/Card';
+import { CardBody } from 'components/Containers/Card/CardBody';
+import { CardHeader } from 'components/Containers/Card/CardHeader';
+import { useGetDevice } from 'hooks/Network/Devices';
+
+type Props = {
+  serialNumber: string;
+};
+
+const RestrictionsCard = ({ serialNumber }: Props) => {
+  const { t } = useTranslation();
+  const getDevice = useGetDevice({ serialNumber });
+
+  const LABELS = {
+    dfs: t('restrictions.dfs'),
+    ssh: 'SSH',
+    rtty: 'RTTY',
+    tty: t('restrictions.tty'),
+    developer: t('restrictions.developer'),
+    upgrade: t('restrictions.signed_upgrade'),
+    commands: t('restrictions.gw_commands'),
+  } as { [key: string]: string };
+
+  const restrictions = getDevice.data?.restrictionDetails;
+
+  if (!restrictions || !getDevice.data?.restrictedDevice) return null;
+
+  const allowed = () => {
+    const allowedKeys = Object.entries(restrictions).filter(([k, v]) => v === false && LABELS[k] !== undefined);
+    return allowedKeys.map(([k]) => <ListItem key={k}>{LABELS[k]}</ListItem>);
+  };
+  const restricted = () => {
+    const restrictedKeys = Object.entries(restrictions).filter(([k, v]) => v === true && LABELS[k] !== undefined);
+    return restrictedKeys.map(([k]) => <ListItem key={k}>{LABELS[k]}</ListItem>);
+  };
+
+  return (
+    <Card mb={4}>
+      <CardHeader>
+        <Heading size="md">{t('restrictions.title')}</Heading>
+      </CardHeader>
+      <CardBody p={0} display="block">
+        <Flex mt={2}>
+          <Heading size="sm" mr={2}>
+            {t('restrictions.countries')}:
+          </Heading>
+          <Text>{restrictions.country.join(', ')}</Text>
+        </Flex>
+        <Heading size="sm" mt={2}>
+          {t('restrictions.key_verification')}
+        </Heading>
+        <UnorderedList>
+          <ListItem>
+            {t('controller.wifi.vendor')}: {restrictions.key_info?.vendor}
+          </ListItem>
+          <ListItem>
+            {t('restrictions.algo')}: {restrictions.key_info?.algo}
+          </ListItem>
+        </UnorderedList>
+        <Flex mt={2}>
+          <Box w="100%">
+            <Flex>
+              <Heading size="sm" mr={2}>
+                {t('restrictions.restricted')}
+              </Heading>
+              <LockSimple size={20} weight="bold" />
+            </Flex>
+            <UnorderedList>{restricted()}</UnorderedList>
+          </Box>
+          <Box w="100%">
+            <Flex>
+              <Heading size="sm" mr={2}>
+                {t('restrictions.allowed')}
+              </Heading>
+              <LockSimpleOpen size={20} weight="bold" />
+            </Flex>
+            <UnorderedList>{allowed()}</UnorderedList>
+          </Box>
+        </Flex>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default RestrictionsCard;
