be3f5548f479a7cd3334382bc1e561ec32c1a73e
Charles <charles.bourque96@gmail.com>
2023-05-03 18:07:41 +0200

    [WIFI-12576] Cache fixes
    Signed-off-by: Charles <charles.bourque96@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,50 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    VitePWA({
+      selfDestroying: true,
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+        /* other options */
+      },
+      manifest: {
+        name: 'OpenWiFi Controller App',
+        short_name: 'OpenWiFiController',
+        description: 'OpenWiFi Controller App',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     outDir: './build',
     chunkSizeWarningLimit: 1000,
